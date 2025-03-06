7cd8c1b9a86ff806a485e4e2c2fad6418c0392ab
Helder Burato Berto <helder.burato@gmail.com>
2025-03-06 21:25:05 +0000

    Update vitest scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
-    include: ['**/*.test.ts'],
-    globals: true
+    globals: true,
+    environment: 'node',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
   },
 })
